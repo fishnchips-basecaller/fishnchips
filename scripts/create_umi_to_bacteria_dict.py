b874427ae59cda68d79900218066d6096402e953
Felix Gravila <felixgravila@gmail.com>
2020-06-10 16:54:24 +0200

    Update paths

--- scripts/create_umi_to_bacteria_dict.py
+++ scripts/create_umi_to_bacteria_dict.py
@@ -1,15 +1,14 @@
 import json
 from sortedcontainers import SortedDict
 
-umireflinkcsvfile = "/mnt/nvme/bio/extracted/umi_ref_link.csv"
-fast5folder = "/mnt/nvme/bio/extracted/fast5"
-tot_folders = 23574 # number of lines in umi ref file
+umireflinkcsvfile = "/path/to/foldertobacteriamapping.csv"
+fast5folder = "/path/to/fast5"
 
 umidict = {}
 
 with open(umireflinkcsvfile, "r") as f:
     for i,line in enumerate(f):
-        print(f"{i:05d}/{tot_folders}", end="\r")
+        print(f"{i:05d}", end="\r")
         umifolder, bact = line.replace("\n", "").split(",")
         with open(f"{fast5folder}/{umifolder}/filename_mapping.txt", "r") as g:
             for sline in g:
